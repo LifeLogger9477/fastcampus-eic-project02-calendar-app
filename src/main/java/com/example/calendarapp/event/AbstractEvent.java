8589c2fa4661a8ef9a4607f1066e51146f287f94
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-02-26 12:26:06 +0900

    Project 02 - 일정 관리 프로그램 만들기 : 공통적을 사용될 abstract class와 meeting class 작성

---
+++ src/main/java/com/example/calendarapp/event/AbstractEvent.java
@@ -0,0 +1,49 @@
+package com.example.calendarapp.event;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-02-26
+ */
+public abstract class AbstractEvent implements Event {
+
+  private final int id;
+  private String title;
+
+  private ZonedDateTime startAt;
+  private ZonedDateTime endAt;
+  private Duration duration;      // 편의를 위해
+
+  private final ZonedDateTime createdAt;
+  private ZonedDateTime modifiedAt;
+
+  private boolean deleteYn;
+
+  // 생성자
+  protected AbstractEvent(
+      int id,
+      String title,
+      ZonedDateTime startAt,
+      ZonedDateTime endAt
+  ) {
+
+    this.id = id;
+    this.title = title;
+    this.startAt = startAt;
+    this.endAt = endAt;
+    this.duration = Duration.between(startAt, endAt);
+
+    ZonedDateTime now = ZonedDateTime.now();
+    this.createdAt = now;
+    this.modifiedAt = now;
+
+    this.deleteYn = false;
+  }
+
+  public String getTitle() {
+
+    return this.title;
+  }
+}
---
+++ src/main/java/com/example/calendarapp/event/Event.java
@@ -0,0 +1,10 @@
+package com.example.calendarapp.event;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-02-26
+ */
+public interface Event {
+
+  void print();
+}
